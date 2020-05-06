b9c9bf166da4641d9365531cb559c8d3b1a1a9af
Leon Gross <leon.gross@rub.de>
2020-05-07 00:11:46 +0100

    Fixed broken device parsing. Now ready to use

--- vera_decrypt.sh
+++ vera_decrypt.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# run script with uid = 1
-# TODO: fstab?
+
+# TDOD: fstab
+# root priv check
+if [ "$(id -u)" -ne 0 ];then
+	printf "[-] Root permissions needed\n"
+	exit 1
+fi
 
 # device uuids
 vera_key_PARTUUID="00873066-01"
@@ -12,21 +17,24 @@ printf "[*] vera_media_dev = %s\n" $vera_media_PARTUUID
 # using veracrypt with keyfile and UUID
 #ext_dev=$(sudo blkid | grep /dev/sd)
 
-vera_key_dev=$(sudo blkid | grep /dev/sd | grep $vera_key_PARTUUID | awk '{ print $1}')
-vera_media_dev=$(sudo blkid | grep /dev/sd | grep $vera_media_PARTUUID | awk '{ print $1}')
+# removing appneded ':'
+vera_key_dev=$(sudo blkid | grep /dev/sd | grep $vera_key_PARTUUID | awk '{ print substr($1, 1, length($1)-1)}')
+vera_media_dev=$(sudo blkid | grep /dev/sd | grep $vera_media_PARTUUID | awk '{ print substr($1, 1, length($1)-1)}')
 
 echo "[+] Key device : $vera_key_dev"
 echo "[+] Media device : $vera_media_dev"
 
 # secure mounting
 [ -d /mnt/vera_key ] && printf "[+] Mountpoint /mnt/vera_key already exists\n" || sudo mkdir /mnt/vera_key
-sudo mount -t vfat $vera_key_dev /mnt/vera_key || echo "[-] Could not mount device $vera_key_dev";
+sudo mount -t vfat $vera_key_dev /mnt/vera_key 2>/dev/null || echo "[-] Could not mount device $vera_key_dev"
 
 [ -d /mnt/vera_media ] && printf "[+] Mountpoint /mnt/vera_media already exists\n" || sudo mkdir /mnt/vera_media
 #sudo mount $vera_media_dev /mnt/vera_media || echo "[-] Could not mount device $vera_media_dev";
 
 # decrypting harddrive using key file
-sudo veracrypt -m ro -k /mnt/vera_key/vera.key --pim=0 --non-interactive $vera_media_dev /mnt/vera_media/ && echo "[+] Successfully decrypted and mounted volume" || echo "[-] Error decrypting volume. See logs"; exit
+# dropping privilieges for mounting
+printf "[+] Starting Volumne Decryption ...\n"
+su $USER -c "veracrypt -m ro -k /mnt/vera_key/vera.key --pim=0 --non-interactive $vera_media_dev /mnt/vera_media/" && echo "[+] Successfully decrypted and mounted volume" || echo "[-] Error decrypting volume. See logs"
 
 # starting samba client
 sudo service smbd status 1>/dev/null && echo "[+] Samba service running" || sudo service sbmd start;
